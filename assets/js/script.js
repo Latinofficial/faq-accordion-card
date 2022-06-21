c41e8a133b03f3cc289a35cc597aa19fc58a0c0b
Latin Official <amzatfridous@gmail.com>
2022-06-21 18:37:12 +0100

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const myName = "Tito";
+
+let is_it_true = false;
+
+// is_it_true = true;
+
+const myNumber = 1234;
+const theNumber = "1234";
+
+// if (myNumber == "1234") {
+//   console.log(myName);
+// } else {
+//   console.log(myNumber);
+// }
+
+const add = theNumber - 1;
+
+console.log(add);
